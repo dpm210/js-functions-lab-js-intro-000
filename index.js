106c5109b145c7316a18c6c7fced147ee8d5a189
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-05 12:24:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // write your code below!
 let name='Dan';
+let holiday='Easter';
 function happyHolidays(string){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
   return`Happy holidays, ${name}!`
 }
-function happyCustomHolidayTo(holiday,name)
+function happyCustomHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
