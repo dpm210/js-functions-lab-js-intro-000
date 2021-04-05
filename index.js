46735c2ab215d1baff60513e286ce2eef5dd2971
Daniel Mueller <danielpetermueller@gmail.com>
2021-04-05 12:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // write your code below!
 let name='Dan';
 let holiday='Easter';
+let days='60'
 function happyHolidays(string){
   return "Happy holidays!"
 }
@@ -9,4 +10,7 @@ function happyHolidaysTo(name){
 }
 function happyCustomHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday,days){
+  return `It/'s ${days} until ${holiday}!`
 }
